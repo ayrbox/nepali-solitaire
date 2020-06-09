41678810776a93b4c8fb0be770e3386555af97e0
ayrbox <srdx7@hotmail.com>
2020-06-09 15:56:06 +0100

    Basic structure of card.

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,23 @@
 import React from "react";
 import clsx from "clsx";
 import cardStyles from "./card.module.scss";
+import Symbols from "./Symbols";
 
-const Card = ({ card, suit }) => {
+const Card = ({ number, suit }) => {
   const cardClass = clsx(cardStyles.card, {
     [cardStyles[suit]]: true
   });
 
   return (
     <div className={cardClass}>
-      <h1>{card}</h1>
-      <p> {suit}</p>
+      <div className={clsx(cardStyles.corner, cardStyles.top)}>
+        <span className={cardStyles.number}>{number}</span>
+        <Symbols suit={suit} />
+      </div>
+      <div className={clsx(cardStyles.corner, cardStyles.bottom)}>
+        <span className={cardStyles.number}>{number}</span>
+        <Symbols suit={suit} />
+      </div>
     </div>
   );
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,36 @@ import React from "react";
 import "../sass/main.scss";
 import Card from "../components/Card";
 
+const cardNumbers = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
+const cardSuits = ["spade", "heart", "club", "diamond"];
+
 const IndexPage = () => {
   return (
     <div>
       <h1>Welcome to Nepali Solitaire</h1>
-      <Card card="A" suit="spade" />
+      <div style={{ fontSize: "12px" }}>
+        {cardSuits.map(suit => (
+          <div key={suit}>
+            {cardNumbers.map(num => (
+              <Card key={num} suit={suit} number={num} />
+            ))}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
